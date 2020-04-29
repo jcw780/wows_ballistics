2ddb32380ef3ee87cb362f9b26879fa878dfda96
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-04-29 19:47:48 -0400

    Updates

---
+++ precache-manifest.5a93e0a9673aab0550933616337db0ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed9211f2ddfefa6fb99da38a415ac50a",
+    "url": "/wows_ballistics_2/index.html"
+  },
+  {
+    "revision": "6bf0935d000e153725df",
+    "url": "/wows_ballistics_2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fe52b09bcdc0fec5a3d9",
+    "url": "/wows_ballistics_2/static/js/2.ae900e67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wows_ballistics_2/static/js/2.ae900e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf0935d000e153725df",
+    "url": "/wows_ballistics_2/static/js/main.1bc4d04a.chunk.js"
+  },
+  {
+    "revision": "984fcb9b5298be3e45c1",
+    "url": "/wows_ballistics_2/static/js/runtime-main.cb8ed3e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wows_ballistics_2/static/media/logo.5d5d9eef.svg"
+  }
+]);
