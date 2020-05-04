c183f211ba346a79210f57fdeb3e096477e9c896
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-05-04 17:04:44 -0400

    Updates

---
+++ precache-manifest.4b9476b6b9e5f0597cd94605b4a2721d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd4463a128536408370b30815c369dc",
+    "url": "/wows_ballistics_2/index.html"
+  },
+  {
+    "revision": "03dfd713940337f5afbd",
+    "url": "/wows_ballistics_2/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "831b44f418ad25eb564f",
+    "url": "/wows_ballistics_2/static/css/main.57f0abae.chunk.css"
+  },
+  {
+    "revision": "03dfd713940337f5afbd",
+    "url": "/wows_ballistics_2/static/js/2.8e5ca5c3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/wows_ballistics_2/static/js/2.8e5ca5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "831b44f418ad25eb564f",
+    "url": "/wows_ballistics_2/static/js/main.f3039970.chunk.js"
+  },
+  {
+    "revision": "984fcb9b5298be3e45c1",
+    "url": "/wows_ballistics_2/static/js/runtime-main.cb8ed3e3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wows_ballistics_2/precache-manifest.e09aafe972aa9e605d86b3612362fc3b.js"
+  "/wows_ballistics_2/precache-manifest.4b9476b6b9e5f0597cd94605b4a2721d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wows_ballistics_2/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
