e9723699666acf15704fc0d03da58b1133cea421
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-05-04 00:25:29 -0400

    moving over to ts

---
+++ src/DefaultForms.tsx
@@ -0,0 +1,195 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+
+class DefaultForm extends React.Component
+<{handleValueChange: Function, controlId: string, label : string, }> {
+	form = React.createRef<HTMLSelectElement>()
+	state = {options: []};
+	constructor(props){
+		super(props);
+		this.state = {options: []};
+		this.form = React.createRef();
+	}
+
+	handleChange = (event) => {
+		event.stopPropagation();
+		this.props.handleValueChange(event.target.value, this.props.controlId);
+	}
+
+	updateOptions = (newOptions) => {
+		this.setState((state) => {
+			return {options: newOptions};
+		});
+	}
+
+	render(){
+		return (
+			<Form.Group className="form-inline">
+				<Form.Label column sm="6">{this.props.label}</Form.Label>
+				<Col sm="2">
+					<Form.Control as="select" placeholder="" 
+					onChange={this.handleChange} ref={this.form}>
+						{this.state.options.map((value ,i) => {return (<option key={i}>{value}</option>);})}
+					</Form.Control>
+				</Col>
+			</Form.Group>
+		);
+	}
+
+	componentDidUpdate(){
+		this.props.handleValueChange(this.form!.current!.value, this.props.controlId);
+	}
+}
+
+const dataURL = "https://jcw780.github.io/wows_ballistics/static/data/"
+
+function fetchJson(target, onSucess){
+    fetch(target)
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(onSucess)
+        .catch((error) => {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    );
+}
+
+function fetchJsonData(target){
+    return fetch(target)
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {return data;})
+        .catch((error) => {
+            console.error('There has been a problem with your fetch operation:', error);
+        }
+    );
+}
+
+export{DefaultForm};
+
+interface defaultFormType{
+	version: [string, string, React.RefObject<DefaultForm>, number];
+	nation: [string, string, React.RefObject<DefaultForm>, number];
+	shipType: [string, string, React.RefObject<DefaultForm>, number];
+	ship: [string, string, React.RefObject<DefaultForm>, number];
+	artillery: [string, string, React.RefObject<DefaultForm>, number];
+	shellType: [string, string, React.RefObject<DefaultForm>, number];
+}
+
+class DefaultShips extends React.Component
+<{sendDefault: Function, }> {
+	defaultForms : defaultFormType = Object.seal({
+		version:   ['Version'   , '', React.createRef<DefaultForm>(), 0],
+		nation:    ['Nation'    , '', React.createRef<DefaultForm>(), 0], 
+		shipType:  ['Type'      , '', React.createRef<DefaultForm>(), 0], 
+		ship:      ['Ship'      , '', React.createRef<DefaultForm>(), 0], 
+		artillery: ['Artillery' , '', React.createRef<DefaultForm>(), 0], 
+		shellType: ['Shell Type', '', React.createRef<DefaultForm>(), 0],
+	})
+	queriedData = {}
+	changeForm = (value, id) => {
+		this.defaultForms[id][1] = value;
+		const queryIndex = this.defaultForms[id][3];
+		switch(queryIndex){
+			case 0:
+				this.queryNation();
+				break;
+			case 1:
+				this.queryType();
+				break;
+			case 2:
+				this.queryShip();
+				break;
+			case 3:
+				this.queryArtillery();
+				break;
+			case 4:
+				this.queryShellType();
+				break;
+			case 5:
+				this.sendData();
+				break;
+			default:
+				break;
+		}
+	}
+	updateForm = (target, options) => {
+		this.defaultForms[target][2].current.updateOptions(options);
+	}
+	queryVersion = () => {
+		const updateForm = this.updateForm;
+		fetchJson(dataURL + "versions.json", function(data){
+			var dataSorted = data.reverse();
+			updateForm('version', dataSorted);
+		});
+	}
+	queryNation = () => {
+		const updateForm = this.updateForm;
+		const previousValue = this.defaultForms.version[1];
+		fetchJson(dataURL + previousValue
+			+ "/nations.json", function (data) {
+			updateForm('nation', data);
+		});
+	}
+	queryType = () => {
+		const updateForm = this.updateForm;
+		const previousValues = [this.defaultForms.version[1], this.defaultForms.nation[1]];
+		fetchJson(dataURL + previousValues[0] + "/" + previousValues[1] + "/shiptypes.json",
+		function (data) {
+			updateForm('shipType', data);
+		});
+	}
+	queryShip = async function() {
+		const previousValues = [this.defaultForms.version[1], this.defaultForms.nation[1], this.defaultForms.shipType[1]];
+		const data = await fetchJsonData(
+			dataURL + previousValues[0] + "/" + previousValues[1] + "/" + previousValues[1] + "_" + previousValues[2] + ".json");
+		this.queriedData = data;
+		var sorted = Object.keys(data);
+		sorted.sort(function(a, b){return data[a]['Tier'] - data[b]['Tier']});
+		this.updateForm('ship', sorted);
+	}
+	queryArtillery = () => {
+		const shipName : string = this.defaultForms.ship[1];
+		const shipInfo = this.queriedData[shipName];
+		var options: string[] = [];
+		Object.keys(shipInfo).map(function(key : string) : void{
+			if(key.includes('Artillery')){
+				options.push(key);
+			}
+		});
+		this.updateForm('artillery', options);
+	}
+	queryShellType = () => {
+		const input = this.queriedData[this.defaultForms.ship[1]][this.defaultForms.artillery[1]];
+		this.updateForm('shellType', Object.keys(input));
+	}
+	sendData = () => {
+		this.props.sendDefault(
+			this.queriedData[this.defaultForms.ship[1]][this.defaultForms.artillery[1]][this.defaultForms.shellType[1]],
+			this.defaultForms.ship[1]
+		);
+	}
+	render(){
+		return(
+			<Form>
+				{Object.entries(this.defaultForms).map( ([k, v], i) => {
+					this.defaultForms[k][3] = i;
+					return (<DefaultForm label={v[0]} key={i} controlId={k}
+					handleValueChange={this.changeForm} ref={v[2]}> </DefaultForm>);
+				})}
+			</Form>
+		);
+	}
+}
+
+export default DefaultShips;
---
+++ src/ShellForms.tsx
@@ -0,0 +1,155 @@
+import React, {SyntheticEvent} from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DefaultShips from './DefaultForms'
+
+
+class ParameterForm extends React.Component
+<{newValue: any, controlId: string, handleValueChange: Function,
+type: string, label: string}>{
+	state = {value: ""};
+	constructor(props){
+		super(props);
+		this.state = {value: this.props.newValue};
+	}
+	handleChange = (event) => {
+		this.props.handleValueChange(event.target.value, this.props.controlId);
+	}
+	updateValue = (newValue) => {
+		this.setState((state) => {
+			return {value: newValue};
+		});
+	}
+	render(){
+		return (
+			<Form.Group className="form-inline">
+				<Form.Label column sm="6">{this.props.label}</Form.Label>
+				<Col sm="2">
+					<Form.Control type={this.props.type} placeholder={this.state.value} 
+					onChange={this.handleChange}/>
+				</Col>
+			</Form.Group>
+		);
+	}
+}
+
+class ShellParameters extends React.Component<{handleValueChange: any, handleNameChange: any,
+	formLabels : any}>{
+	nameForm = React.createRef<ParameterForm>()
+	constructor(props){
+		super(props);
+	}
+	handleValueChange = (value, k) => {
+		this.props.handleValueChange(value, k);
+	}
+	handleNameChange = (event : any) => {
+		this.props.handleNameChange(event.target.value);
+	}
+	updateShells(name : string) {
+		Object.entries(this.props.formLabels).map(function (kv : any): void{
+			const value = kv[1]; 
+			value[2].current.updateValue(value[1]);
+		});
+		const updateValueTemp = this.nameForm.current!.updateValue;
+		updateValueTemp(name);
+	}
+	render() {
+		return(
+			<Form>
+				<ParameterForm label="Ship Label" controlId='shipName'
+						handleValueChange={this.handleValueChange}
+						type="text" newValue=""
+						ref={this.nameForm}></ParameterForm>
+				{Object.entries(this.props.formLabels).map((kv : any, i) => {
+					const key: string = kv[0];
+					const value = kv[1];
+					return (<ParameterForm label={value[0]} key={i} controlId={key}
+					handleValueChange={this.handleValueChange}
+					type="number" newValue={value[1]}
+					ref={this.props.formLabels[key][2]}></ParameterForm>);
+				})}	
+			</Form>
+		);
+	}
+}
+
+class ShellForms extends React.Component<{index: string | number}> {
+	parameters = React.createRef<ShellParameters>()
+	defaults = React.createRef<DefaultShips>()
+	values = Object.seal({
+		caliber: ['Caliber (m)', 0, React.createRef()], 
+		muzzleVelocity: ['Muzzle Velocity', 0, React.createRef()], 
+		dragCoefficient: ['Drag Coefficient', 0, React.createRef()],
+		mass: ['Mass (kg)', 0, React.createRef()], 
+		krupp: ['Krupp', 0, React.createRef()], 
+		fusetime: ['Fusetime (s)', 0, React.createRef()], 
+		threshold: ['Fusing Threshold (mm)', 0, React.createRef()], 
+		normalization: ['Normalization (°)', 0, React.createRef()], 
+		ra0: ['Start Ricochet (°)', 0, React.createRef()], 
+		ra1: ['Always Ricochet (°)', 0, React.createRef()], 
+		HESAP: ['HE/SAP penetration (mm)', 0, React.createRef()],
+	})
+	name = ''
+	constructor(props){
+		super(props);
+	}
+	handleValueChange = (value : string, k : string) => {
+		this.values[k][1] = parseFloat(value);
+	}
+	handleNameChange = (value) => {
+		//this.values.name = value;
+	}
+	getDefaultData = (data, name) => {
+		this.values.caliber[1] = data.bulletDiametr;
+		this.values.muzzleVelocity[1] = data.bulletSpeed;
+		this.values.dragCoefficient[1] = data.bulletAirDrag;
+		this.values.mass[1] = data.bulletMass;
+		this.values.krupp[1] = data.bulletKrupp;
+		this.values.fusetime[1] = data.bulletDetonator;
+		this.values.threshold[1] = data.bulletDetonatorThreshold;
+		this.values.normalization[1] = data.bulletCapNormalizeMaxAngle;
+		this.values.ra0[1] = data.bulletRicochetAt;
+		this.values.ra1[1] = data.bulletAlwaysRicochetAt;
+		this.values.HESAP[1] = data.alphaPiercingHE;
+
+		if(data.alphaPiercingCS > this.values.HESAP[1]){
+			this.values.HESAP[1] = data.alphaPiercingCS;
+		}
+		if(this.parameters){
+			this.parameters.current!.updateShells(name);
+		}
+	}
+	render() {
+		return(
+			<Col sm='4'>
+				<h4>Ship {this.props.index}</h4>
+				<DefaultShips sendDefault={this.getDefaultData} ref={this.defaults}/>
+				<Dropdown>
+					<Dropdown.Toggle variant="success" id="dropdown-basic">
+					Show Detailed Parameters
+					</Dropdown.Toggle>
+
+					<Dropdown.Menu>
+						<Dropdown.Item>
+                            <Col sm="12">
+                            <ShellParameters handleValueChange={this.handleValueChange}
+                                    handleNameChange={this.handleNameChange}
+                                    formLabels={this.values} ref={this.parameters}/>
+                            </Col>
+						</Dropdown.Item>
+					</Dropdown.Menu>
+				</Dropdown>
+			</Col>
+		);
+	}
+	componentDidMount(){
+		this.defaults.current!.queryVersion();
+	}
+}
+
+export default ShellForms;
