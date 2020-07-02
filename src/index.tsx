80c058874798476f7e91168139324ef4d8aaab4a
jwc780 <chengwjin32666@gmail.com>
2020-07-02 12:31:28 -0400

    fix: moved analytics to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
+
 import 'semantic-ui-css/semantic.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('UA-155844497-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const rootElement = document.getElementById('root');
 if(rootElement!.hasChildNodes()){
