d0d64b958019f4f774ce937e95206da9a076dd44
jcw780 <30562395+jcw780@users.noreply.github.com>
2020-11-05 15:52:30 -0500

    added github and discord symbols to navbar w/ corresponding links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -72,6 +72,14 @@ export class NavbarCustom extends React.Component<{links: T.linkT}>{
             />
         </Nav>
         <Nav>
+            <Nav.Link href="https://discord.gg/fpDB9y5">
+                <img height='32' width='32' alt="logo"
+                src={process.env.PUBLIC_URL + '/Discord-Logo-White.png'}/>
+            </Nav.Link>
+            <Nav.Link href="https://github.com/jcw780/wows_ballistics">
+                <img height='32' width='32' alt="logo"
+                src={process.env.PUBLIC_URL + '/Github-Mark-Light-32px.png'}/>
+            </Nav.Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <NavDropdown title="Support" id="support" alignRight>
                     <ND.Item href="https://discord.gg/fpDB9y5">Discord</ND.Item>
